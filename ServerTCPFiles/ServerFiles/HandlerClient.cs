7c4a022b4efe9b93e80bec9663a942634a2b1034
Oscar Ivan Hernandez Vazquez <oscar.i.hdz.v@hotmail.com>
2019-06-14 00:33:34 -0500

    Proyecto Inicial FilesServer

---
+++ ServerTCPFiles/ServerFiles/HandlerClient.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// using utilizados para Sockets
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ServerFiles
+{
+    class HandlerClient
+    {
+
+        #region Variables
+        TcpClient clientSocket;
+
+        #endregion
+
+        #region Métodos
+
+        public void StartClient(TcpClient inClientSocket)
+        {
+            this.clientSocket = inClientSocket;;
+            
+            try
+            {
+                Thread ctThread = new Thread(DoAction);
+                ctThread.Start();
+            }
+            catch (ThreadStartException)
+            {
+                throw new Exception("Error al iniciar servicio del thread.");
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error al iniciar servicio en general.");
+            }
+
+        }
+
+        private void DoAction()
+        {
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+
+            while ((true))
+            {
+                try
+                {
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                    
+                    serverResponse = "Server to clinet('Hi this is my response')";
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+                catch(ObjectDisposedException)
+                {
+                    Console.WriteLine("El NetworkStream del client esta vacío.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" >> " + ex.ToString());
+                } // try - finally
+            } // while
+        } // DoAction
+        #endregion
+    }
+}
---
+++ ServerTCPFiles/ServerFiles/ServerFile.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// using utilizados para Sockets
+using System.Net;
+using System.Net.Sockets;
+
+//Multi-Thread
+using System.Threading;
+namespace ServerFiles
+{
+    class ServerFile
+    {
+        #region Variables
+        TcpListener serverSocket;
+        TcpClient clientSocket;
+        #endregion
+
+        #region Propiedades
+        public int Port { set; get; }
+        public string Server { set; get; }
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Constructor vacío.
+        /// </summary>
+        public ServerFile(){}
+
+        /// <summary>
+        /// Constructor con puerto indicado.
+        /// </summary>
+        /// <param name="aPort"> Puerto sobre el que se levantará el servidor.</param>
+        public ServerFile(int aPort)
+        {
+            this.Port = aPort;
+        }
+        #endregion
+
+        #region Métodos
+        public void Start()
+        {
+            try
+            {
+                serverSocket = new TcpListener(IPAddress.Parse(Server), Port);
+                clientSocket = default(TcpClient);
+
+                serverSocket.Start();
+
+                // Aceptamos la conexión del cliente y la manejamos en otro Thread.
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                HandlerClient hndlCliente = new HandlerClient();
+                hndlCliente.StartClient(clientSocket);
+
+                clientSocket.Close();
+                serverSocket.Stop();
+                Console.WriteLine("Exiting...");
+
+            }
+            catch (SocketException Arg)
+            {
+                Console.WriteLine(Arg.Message);
+            }
+            catch (ArgumentNullException Arg)
+            {
+                Console.WriteLine(Arg.Message);
+            } // try - finally
+        }
+        #endregion
+    }
+}
