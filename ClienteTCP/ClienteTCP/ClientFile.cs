071836d87b7aa7b00aada1e2ab90da689996ac61
Oscar Ivan Hernandez Vazquez <oscar.i.hdz.v@hotmail.com>
2019-06-14 22:54:22 -0500

    Se crea el método enviar. Es la parte más simple para enviar un texto al server y recibir una respuesta.

--- ClienteTCP/ClienteTCP/ClientFile.cs
+++ ClienteTCP/ClienteTCP/ClientFile.cs
@@ -23,8 +23,8 @@ namespace ClienteTCP
 
         #region Variables
 
-        TcpClient socketCliente;
-
+        TcpClient clientSocket = new TcpClient();
+        NetworkStream serverStream;
         #endregion
 
         #region Métodos
@@ -36,6 +36,38 @@ namespace ClienteTCP
             Server = aServer;
         }
         public ClientFile() { }
+
+
+        public void Conectar()
+        {
+            try
+            {
+                clientSocket.Connect(Server, Port);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Ocurrió un problema con el socket");
+                throw new Exception("Problema en el socket.");
+            }
+            catch (ObjectDisposedException)
+            {
+                Console.WriteLine("El socket cliente se encuentra null");
+                throw new Exception("El socket cliente no esta creado.");
+            }
+        }
+
+        public void Enviar()
+        {
+
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = Encoding.ASCII.GetBytes("Message from Client$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            string returndata = Encoding.ASCII.GetString(inStream);
+        }
 
         #endregion
     }
